f55866779ac0d877e5a458faf90cc62a82685759
Jona727 <jonaram727@gmail.com>
2025-05-09 19:50:16 -0300

    Update userdetail.py

--- routes/userdetail.py
+++ routes/userdetail.py
@@ -1,1 +1,23 @@
+from fastapi import APIRouter
+from models.userDetailModel import session, UserDetail, InputUserDetail
 
+
+userDetail = APIRouter()
+
+
+@userDetail.get("/userdetail/all")
+def get_userDetails():
+   try:
+       return session.query(UserDetail).all()
+   except Exception as e:
+       print(e)
+
+
+@userDetail.post("/userdetail/add")
+def add_usuarDetail(userDet: InputUserDetail):
+   usuNuevo = UserDetail(
+   userDet.dni, userDet.firstName, userDet.lastName, userDet.type,           userDet.email
+   )
+   session.add(usuNuevo)
+   session.commit()
+   return "usuario detail agregado"
