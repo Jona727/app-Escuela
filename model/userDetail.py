a5bd4a69f5f34c04f1eb18c55f970669197cf29c
Jona727 <jonaram727@gmail.com>
2025-05-09 19:45:01 -0300

    Update userDetail.py

--- model/userDetail.py
+++ model/userDetail.py
@@ -1,1 +1,40 @@
+from config.db import engine, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
 
+class UserDetail(Base):
+
+   __tablename__ = "userdetails"
+
+
+   id = Column("id", Integer, primary_key=True)
+   dni = Column("dni", Integer)
+   firstName = Column("firstName", String)
+   lastName = Column("lastName", String)
+   type = Column("type", String)
+   email = Column("email", String(80), nullable=False, unique=True)
+
+
+   def __init__(self, dni, firstName, lastName, type, email):
+       self.dni = dni
+       self.firstName = firstName
+       self.lastName = lastName
+       self.type = type
+       self.email = email
+
+
+Base.metadata.drop_all(bind=engine) # Ojo, dropeará todas las BD primero
+Base.metadata.create_all(bind=engine)
+
+
+Session = sessionmaker(bind=engine)  # creo una clase session
+session = Session()
+
+
+class InputUserDetail(BaseModel):
+   dni: int
+   firstName: str
+   lastName: str
+   type: str
+   email: str
